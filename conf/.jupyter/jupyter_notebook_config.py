db41744f8a90726bd2442794316f0e8de37b086e
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-02-04 18:44:17 -0500

    Fixing image startup configuration problem

--- conf/.jupyter/jupyter_notebook_config.py
+++ conf/.jupyter/jupyter_notebook_config.py
@@ -1,13 +1,14 @@
 c = get_config()  # get the config object
 c.IPKernelApp.pylab = 'inline'  # in-line figure when using Matplotlib
-c.NotebookApp.ip = '*'  
+c.NotebookApp.ip = '*'
+c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False  # do not open a browser window by default when using notebooks
 c.NotebookApp.token = '' # No token. Always use jupyter over ssh tunnel
 c.NotebookApp.notebook_dir = '/notebooks'
 c.NotebookApp.allow_root = True # Allow to run Jupyter from root user inside Docker container
 c.NotebookApp.allow_origin = '*'
-c.NotebookApp.tornado_settings = { 
-    'headers': { 
-        'Content-Security-Policy': "frame-ancestors 'self' *" 
+c.NotebookApp.tornado_settings = {
+    'headers': {
+        'Content-Security-Policy': "frame-ancestors 'self' *"
     }
 }
